ceefe4f1b060f1870ae2352815539eeb0e6ddf07
Mori Baptiste <21602052@C304L-403C16.campus.unicaen.fr>
2019-01-18 12:17:26 +0100

    fin sceance tp

---
+++ VentesImmobilieres/app/build/generated/source/buildConfig/debug/fr/unicaen/info/users/a21606807/ventesimmobilires/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fr.unicaen.info.users.a21606807.ventesimmobilires;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "fr.unicaen.info.users.a21606807.ventesimmobilires";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}

4af880fff1074ea32b3b9ff0254d669317c34b4a
ValentinLe <21609038@etu.unicaen.fr>
2019-01-27 20:04:49 +0100

    propriete par default quand on n'a pas d'extra pour AnnonceActivity

--- VentesImmobilieres/app/src/main/java/fr/unicaen/info/users/a21606807/ventesimmobilires/ActivityAnnonce.java
+++ VentesImmobilieres/app/src/main/java/fr/unicaen/info/users/a21606807/ventesimmobilires/ActivityAnnonce.java
@@ -1,5 +1,6 @@
 package fr.unicaen.info.users.a21606807.ventesimmobilires;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -38,29 +39,36 @@ public class ActivityAnnonce extends AppCompatActivity {
 
         ArrayList<String> images = new ArrayList<>();
         images.add("https://www.villadeale.fr/media/thumbnails/villatango_3_chambres_gd__032041100_1112_31102017.jpg");
-        /*
-        Propriete propriete = new Propriete(
-                "1",
-                "Maison SWAG",
-                "Super maison trop géniale !",
-                5,
-                new ArrayList<String>(),
-                1005600,
-                "Moncuq",
-                new Vendeur(
-                        "76",
-                        "Marley",
-                        "Bob",
-                        "Boby@gmail.com",
-                        "0215653289"
-                ),
-                images,
-                new Date()
-        );*/
-        Propriete propriete = getIntent().getParcelableExtra("propriete");
+
+        Propriete propriete;
+
+        Intent intent = getIntent();
+        if (intent.getParcelableExtra("propriete") != null) {
+            propriete = getIntent().getParcelableExtra("propriete");
+        } else {
+            propriete = new Propriete(
+                    "1",
+                    "Maison SWAG",
+                    "Super maison trop TROP géniale !",
+                    5,
+                    new ArrayList<String>(),
+                    1005600,
+                    "Moncuq",
+                    new Vendeur(
+                            "76",
+                            "Marley",
+                            "Bob",
+                            "Boby@gmail.com",
+                            "0215653289"
+                    ),
+                    images,
+                    new Date()
+            );
+        }
+
         this.fillAnnonce(propriete);
 
-        //this.getPropriete("https://ensweb.users.info.unicaen.fr/android-estate/mock-api/immobilier.json");
+        this.getPropriete("https://ensweb.users.info.unicaen.fr/android-estate/mock-api/immobilier.json");
     }
 
     public void fillAnnonce(Propriete propriete) {
@@ -97,13 +105,17 @@ public class ActivityAnnonce extends AppCompatActivity {
                     if (!response.isSuccessful()) {
                         throw new IOException("Unexpected HTTP code " + response);
                     }
-                    Moshi moshi = new Moshi.Builder()
-                            .add(new ProrieteAdapter())
-                            .build();
-                    JsonAdapter<Propriete> adapter = moshi.adapter(Propriete.class);
+
+                    Moshi moshi = new Moshi.Builder().build();
+
                     Log.i("val", "PASSAGE ICI");
+                    JsonAdapter<Propriete> adapter = moshi.adapter(Propriete.class);
+                    Log.i("val", "PASSAGE LA !!!!");
+
                     Propriete prop = adapter.fromJson(responseBody.string());
+                    Log.i("val", "ENFIN !!!");
                     Log.i("val", prop.toString());
+
                 }
             }
         });
